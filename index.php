58a3989a7707cc3df5c3a04e0cda25c1d55c7bb5
Veraghe <alison231193@hotmail.com>
2020-05-13 14:50:21 +0200

    base

---
+++ index.php
@@ -0,0 +1,29 @@
+<?PHP
+
+function ChargerClasse($classe)
+{
+    if (file_exists("PHP/CONTROLLER/" . $classe . ".Class.php")) {
+        require "PHP/CONTROLLER/" . $classe . ".Class.php";
+    }
+    if (file_exists("PHP/MODEL/" . $classe . ".Class.php")) {
+        require "PHP/MODEL/" . $classe . ".Class.php";
+    }
+}
+spl_autoload_register("ChargerClasse");
+function AfficherPage($page)
+{
+    $chemin = $page[0];
+    $nom = $page[1];
+    $titre = $page[2];
+
+    include 'PHP/VIEW/Head.php';
+    include 'PHP/VIEW/Header.php';
+    include $chemin . $nom . '.php'; //Chargement de la page en fonction du chemin et du nom
+    include 'PHP/VIEW/Footer.php';
+}
+
+// on initialise les paramètres du fichier parametre.ini
+Parametre::init();
+//on active la connexion à la base de données
+DbConnect::init();
+session_start();
