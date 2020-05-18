2cb4c2224c84b22a52d820d22ce650e6cb178869
Ben974 <benjaminfontainedw@gmail.com>
2020-05-18 18:06:54 +0200

    modif

--- PHP/MODEL/DbConnect.class.php
+++ PHP/MODEL/DbConnect.class.php
@@ -9,7 +9,7 @@ class DbConnect {
 	public static function init() {
 		try {
 			// On se connecte � MySQL
-			self::$db= new PDO ('mysql:host=localhost;port=8888;dbname=calendrier;charset=utf8','root','root');
+			self::$db= new PDO ('mysql:host=localhost;port=3306;dbname=calendrier;charset=utf8','root','');
 		} catch ( Exception $e ) {
 			// En cas d'erreur, on affiche un message et on arr�te tout
 			die ( 'Erreur : ' . $e->getMessage () );
