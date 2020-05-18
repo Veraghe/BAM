664358c2becf6431a47afa8cd6b8821d7d00dd5c
Ben974 <benjaminfontainedw@gmail.com>
2020-05-18 12:15:33 +0200

    inscirption

---
+++ PHP/VIEW/inscription.php
@@ -0,0 +1,54 @@
+<?php
+if(!isset($_POST['email']))
+{
+    include 'PHP/View/inscirptionHTML.php';
+}
+else
+{
+    $erreur ="";
+    $i=0;
+
+    $pseudo=$_POST['pseudo'];
+    $password=md5($_POST['password']);
+    $confirm=md5($_POST['confirm']);
+
+    //on vérifie que le pseudo n'est pas dèjà utilisé
+    $utilisateur=UtilisateurManager::getByPseudo($pseudo);
+    if($pseudo->getPseudo()!=null)
+    {
+        $erreur .= "Ce Pseudo est déjà utlisé, veuillez en choisir un autre";
+        $i++;
+        //on vérifie que l'email n'existe pas déjà
+        if($pseudo->getEmailUtilisateur()!=null)
+        {
+            $erreur .="Cet email est déjà utlisé";
+            $i++;
+        }
+    }
+    //verification du mdp
+    if ($password != $confirm|| empty($confirm)|| empty($password))
+    {
+        $erreur .=" Le mot de passe est incorrect ou la confirmation ne correspond pas au mot de passe";
+        $i++;
+    }
+    if($i==0) // si il n'y a pas d'erreurs
+    {
+        $nouveauUtilisateur= new Utilisateur(['pseudo'=>$_POST['pseudo'], 'motDePasse'=>md5($_POST['password']),'nomUtilisateur'=>$_POST['nom'], 'prenomUtilisateur'=>$_POST['prenomUtilisateur'],'emailUtilisateur'=>$_POST['emailUtilisateur'],'telephoneUtilisateur'=>$_POST['telephoneUtilisateur']]);
+        
+        UtilisateurManager::add($nouveauUtilisateur);
+
+        $nouveauUtilisateur=UtilisateurManager::getByPseudo($_POST['pseudo']);
+        echo '<p>Bienvenue'.$nouveauUtilisateur->getPseudo().', vous êtes maintenant inscrit.e </p>';
+
+        $_SESSION['pseudo']= $nouveauUtilisateur->getPseudo();
+        $_SESSION['nomUtilisateur']= $nouveauUtilisateur->getNomUtilisateur();
+        $_SESSION['prenomUtilisateur']= $nouveauUtilisateur->getPrenomUtilisateur();
+        header('url=index.php?action=default');
+    }
+    else 
+    {
+        echo '<p> Une ou plusieurs erreurs se sont produites durant l\'inscription</p>';
+        echo '<p>'.$erreur.'</p>';
+        header("url=index.php?action=inscription");
+    }    
+}
---
+++ PHP/VIEW/inscriptionHTML.php
@@ -0,0 +1,45 @@
+<body>
+    <div class="titre">
+        <p>Inscription</p>
+    </div>
+    <div class="formulaire">
+        <form method="post" action="index.php?action=">    
+            <fieldset>
+                <legend>Inscription</legend>
+                <div class="colonne">
+                    <div> 
+                        <label for="pseudo">Pseudo :</label>
+                        <input name="pseudo" type="text" id="pseudo" required pattern/>
+                    </div>
+                    <div> 
+                        <label for="motDePasse">Mot de passe : </label>
+                        <input type="password" id="motDePasse" name="motDePasse" required pattern/>
+                    </div>
+                    <div> 
+                        <label for="nomUtilisateur">Nom :</label>
+                        <input name="nomUtilisateur" type="text" id="nomUtilisateur" required pattern/>
+                    </div>
+                    <div> 
+                        <label for="confirm">Confirmation mot de passe : </label>
+                        <input type="password" id="confirm" name="confirm" required pattern/>
+                    </div>
+                    <div> 
+                        <label for="prenomUtilisateur">Prenom :</label>
+                        <input name="prenomUtilisateur" type="text" id="prenomUtilisateur" required pattern/>
+                    </div>
+                    <div> 
+                        <label for="emailUtilisateur">Mail : </label>
+                        <input type="mail" id="emailUtilisateur" name="emailUtilisateur" required pattern/>
+                    </div>
+                    <div> 
+                        <label for="telephoneUtilisateur">Téléphone :</label>
+                        <input name="telephoneUtilisateur" type="number" id="telephoneUtilisateur" required pattern/>
+                    </div>
+                </div>
+            </fieldset> 
+            <div class="centrer">
+                <input class="bouton centrer" type="submit" value="Valider" />
+            </div>
+        </form>
+    </div>
+</body>
